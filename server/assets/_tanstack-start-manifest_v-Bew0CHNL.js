a04776a4451071d95e92eb4ed60f493eb1b7ac62
ali-touheed123 <alitouheed.2005@gmail.com>
2026-05-16 10:10:16 +0500

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-Bew0CHNL.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/KING/Desktop/story-weaver-studio/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/birthday-surprise-for-the-love-of-my-life/assets/index-Bg2GCe0N.js"] }, "/": { filePath: "C:/Users/KING/Desktop/story-weaver-studio/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/birthday-surprise-for-the-love-of-my-life/assets/index-oxBaQBTr.js"] } }, clientEntry: "/birthday-surprise-for-the-love-of-my-life/assets/index-Bg2GCe0N.js" });
+export {
+  tsrStartManifest
+};
---
+++ server/index.js
@@ -0,0 +1,5 @@
+import { w } from "./assets/worker-entry-CRt8-Ae2.js";
+import "node:events";
+export {
+  w as default
+};
